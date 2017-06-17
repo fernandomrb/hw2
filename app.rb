18edf583129835e9f1a5b2bc91d4ce73b319eb5c
Fernando Ramos <fernan_moises1@hotmail.com>
2017-06-17 16:43:51 +0000

    Add rerun gem

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
 	get '/' do
-		"<!DOCTYPE html><html><head></head><body>Hello World</body></html>"
+		"<!DOCTYPE html><html><head></head><body>Hello World Again</body></html>"
 	end
 end
